afb30393d30d30a87011ef95bd36f56a4acb495b
Avner Shahar-Kashtan <avner@strawjackal.org>
2018-07-23 09:44:50 +0300

    built FromQuery repro

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetControllerTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        // This will provide an empty array in `user` when calling /api/values/implicit?user=a
+        [HttpGet("implicit")]
+        public ActionResult<IEnumerable<string>> GetImplict([FromQuery] string[] user)
+        {
+            return user;
+        }
+
+        // This will provide the expected values in `user` when calling /api/values/explicit?user=a
+        [HttpGet("explicit")]
+        public ActionResult<IEnumerable<string>> GetExplicit([FromQuery(Name = "user")] string[] user)
+        {
+            return user;
+        }
+    }
+}
